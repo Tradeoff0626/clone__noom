4a933f751556e1d2434d86b653e730e2602c2a82
k.c.Kang <kckang.dev@gmail.com>
2023-02-03 23:20:01 +0900

    추가 설정(기타 및 프론트엔드)

--- src/server.js
+++ src/server.js
@@ -2,5 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "pug");              //뷰 엔진을 pug로 설정
+app.set("views", __dirname + "/views");     //뷰 파일 디렉토리를 /src/views로 설정
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));   //'/'의 렌더링은 home.pug로
+app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log("Listening on http://localhost:3000");
 app.listen(3000, handleListen);
