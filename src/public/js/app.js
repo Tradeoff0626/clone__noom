e2357435f725922880ed582d07b66bfd7c0ee190
k.c.Kang <kckang.dev@gmail.com>
2023-02-04 23:02:11 +0900

    echo 서버 기능 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,31 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+
+socket.addEventListener("open", () => {
+  console.log("Connection to Server");
+})
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server");
+})
+
+/**
+ * 메시지 수신 시 처리
+ */
+socket.addEventListener("message", (message) => {
+  console.log("Just got this:", message.data, "from the server");
+})
+
+/**
+ * form submit 기능 구현
+ */
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+})
--- src/server.js
+++ src/server.js
@@ -18,10 +18,17 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-	console.log(socket);
-}
+/**
+ * 웹소켓 서버 연결 시
+ */
+wss.on("connection", (socket) => {
+  console.log("Connection to Browser");
+  
+  socket.on("close", () => console.log("Disconnected from Browser"));
 
-wss.on("connection", handleConnection);
+  socket.on("message", (msg) => {
+    socket.send(`${msg}`);          //메시지 echo
+  })
+})
 
 server.listen(3000, handleListen);    //http 프로토콜과 ws 프로토콜 모두 사용 
